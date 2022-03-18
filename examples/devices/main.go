6dd7ddcbdacb9cfff820e854a1b8548ae1b54c76
launch100 <launch100@gmail.com>
2022-03-18 14:57:07 -0400

    launch100

--- examples/devices/main.go
+++ examples/devices/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 
-	webexteams "github.com/jbogarin/go-cisco-webex-teams/sdk"
+	webexteams "github.com/launch100/go-cisco-webex-teams/sdk"
 )
 
 // Client is Webex Teams API client
